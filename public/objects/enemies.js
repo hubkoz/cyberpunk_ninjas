954b474d9aa37b66ef3901ec4c06b1941678ee58
Hubert <hubert.kozlowski.1998@gmail.com>
2023-04-12 18:34:47 +0200

    changed img paths to absolute

--- public/objects/enemies.js
+++ public/objects/enemies.js
@@ -1,97 +1,97 @@
 /* ENEMIES */
 const hulk = {
     name: "Hulk",
-    imgSrc: "../src/img/enemies/enemy1.png",
+    imgSrc: "/src/img/enemies/enemy1.png",
     energy: 10,
     prize: 50,
 };
 const chicken = {
     name: "Chicken",
-    imgSrc: "../src/img/enemies/chicken.png",
+    imgSrc: "/src/img/enemies/chicken.png",
     energy: 50,
     prize: 200,
 };
 const hologram = {
     name: "Hologram",
-    imgSrc: "../src/img/enemies/hologram.png",
+    imgSrc: "/src/img/enemies/hologram.png",
     energy: 200,
     prize: 300,
 };
 const robot = {
     name: "Robot",
-    imgSrc: "../src/img/enemies/robot.png",
+    imgSrc: "/src/img/enemies/robot.png",
     energy: 500,
     prize: 500,
 };
 const eagle = {
     name: "Eagle",
-    imgSrc: "../src/img/enemies/eagle.png",
+    imgSrc: "/src/img/enemies/eagle.png",
     energy: 1000,
     prize: 800,
 };
 const bear = {
     name: "Teddy Bear",
-    imgSrc: "../src/img/enemies/teddybear.png",
+    imgSrc: "/src/img/enemies/teddybear.png",
     energy: 3000,
     prize: 1500,
 };
 const puma = {
     name: "Puma",
-    imgSrc: "../src/img/enemies/puma.png",
+    imgSrc: "/src/img/enemies/puma.png",
     energy: 8000,
     prize: 3800,
 };
 const snake = {
     name: "Snake",
-    imgSrc: "../src/img/enemies/snake.png",
+    imgSrc: "/src/img/enemies/snake.png",
     energy: 15000,
     prize: 8000,
 };
 const nomad = {
     name: "Digital Nomad",
-    imgSrc: "../src/img/enemies/nomad.png",
+    imgSrc: "/src/img/enemies/nomad.png",
     energy: 30000,
     prize: 16000,
 };
 const octopus = {
     name: "Octopus",
-    imgSrc: "../src/img/enemies/octopus.png",
+    imgSrc: "/src/img/enemies/octopus.png",
     energy: 50000,
     prize: 25000,
 };
 const cyborg = {
     name: "Cyborg 100%",
-    imgSrc: "../src/img/enemies/cyborg.png",
+    imgSrc: "/src/img/enemies/cyborg.png",
     energy: 150000,
     prize: 50000,
 };
 const emoji = {
     name: "Angry Emoji",
-    imgSrc: "../src/img/enemies/emoji.png",
+    imgSrc: "/src/img/enemies/emoji.png",
     energy: 250000,
     prize: 100420,
 };
 const cop = {
     name: "Corrupt Cop",
-    imgSrc: "../src/img/enemies/cop.png",
+    imgSrc: "/src/img/enemies/cop.png",
     energy: 500000,
     prize: 997112,
 };
 const samurai = {
     name: "Samurai",
-    imgSrc: "../src/img/enemies/samurai.png",
+    imgSrc: "/src/img/enemies/samurai.png",
     energy: 850000,
     prize: 2000000,
 };
 const terminator = {
     name: "Terminator",
-    imgSrc: "../src/img/enemies/terminator.png",
+    imgSrc: "/src/img/enemies/terminator.png",
     energy: 9200006,
     prize: 7777777,
 };
 const duck = {
     name: "Devil Duck",
-    imgSrc: "../src/img/enemies/duck.png",
+    imgSrc: "/src/img/enemies/duck.png",
     energy: 5000000,
     prize: 5000000,
 };
